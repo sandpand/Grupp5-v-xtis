45bed6b4b5466144a7f3203e89e9a6c395b3be9e
Rappich <fredrik.martensson@chasacademy.se>
2025-03-21 12:07:02 +0100

    changes

--- src/Actuators/WaterPump.cpp
+++ src/Actuators/WaterPump.cpp
@@ -1,5 +1,6 @@
-"include <Actuators/WaterPump.h"
+#include <include/Actuators/WaterPump.h>
 
-void WaterPump::setSpeed(int speed) {
-    analogWrite(WATER_PUMP_PIN, speed);  // Set the water pump speed (0-255 range)
+void WaterPump::setSpeed(int speed)
+{
+    analogWrite(WATER_PUMP_PIN, speed); // Set the water pump speed (0-255 range)
 }
